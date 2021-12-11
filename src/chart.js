a781f6669d240cd56e6d5ccadd6f8a1e6c9870ab
kevser-tr <kevser.tanrikulu@student.ehb.be>
2021-12-11 13:01:08 +0100

    npm installed + webpack

---
+++ src/chart.js
@@ -0,0 +1,79 @@
+import Chart from 'chart.js/auto';
+
+
+const Chart = require('chart.js');
+const myChart = new Chart(ctx, {
+  ...
+});
+
+
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+} from 'chart.js';
+
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+);
+
+const myChart = new Chart(ctx, {
+  ...
+});
+
+import {
+  Chart,
+  registerables
+} from 'chart.js';
+Chart.register(...registerables);
+
+require(['path/to/chartjs/dist/chart.min.js'], function (Chart) {
+  const myChart = new Chart(ctx, {
+    ...
+  });
+});
